27e24da0585e6d296d168f331f5fe438f4c56512
Tejeswar V <vtejaswar@gmail.com>
2020-09-22 23:36:07 +0530

    Added daily wage and switch case

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -10,20 +10,11 @@ absent=0
 
 check=$((RANDOM%3))
 
-if [[ $check == 1 ]]
-then
-	emp_work_hour=4
-	echo "Employee is present part-time"
-elif [[ $check == 2 ]]
-then
-	emp_work_hour=8
-	echo "Employee is present full-time"
-else
-	emp_work_hour=0
-	echo "Employee is absent"
-
-fi
-
+case $check in
+	1)emp_work_hour=4;;
+	2)emp_work_hour=8;;
+	*)emp_work_hour=0;;
+esac
 
 dailyEmpWage()
 {
