9b0ad747f373d24433c8accc9e72577ff0f2831f
Tejeswar V <vtejaswar@gmail.com>
2020-09-22 22:58:04 +0530

    Added Check for Attendace

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
 echo "Welcome to employee wage computation program"
 
-check=$((RANDOM%2))
-if [[ $check -eq 1 ]]
+
+
+check=$((RANDOM%3))
+
+if [[ $check == 1 ]]
 then
-	echo "Employee is present"
+	emp_work_hour=4
+	echo "Employee is present part-time"
+elif [[ $check == 2 ]]
+then
+	emp_work_hour=8
+	echo "Employee is present full-time"
 else
+	emp_work_hour=0
 	echo "Employee is absent"
+
 fi
